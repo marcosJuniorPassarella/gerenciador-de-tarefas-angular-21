6e7c83d22ff85f9aa8b565d2e61144dc8fd56191
Marcos Júnior Passarella <mjworks2020@outlook.com>
2026-05-14 13:55:05 -0300

    Aula 5 - Parte 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { TaskFiltersComponent } from './components/task-filters/task-filters.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, TaskFiltersComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
--- src/app/components/task-filters/task-filters.component.ts
+++ src/app/components/task-filters/task-filters.component.ts
@@ -1,11 +1,26 @@
 import { TaskPriority, TaskStatus } from './../../models/task.model';
 import { Component, computed, inject } from '@angular/core';
 import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-filters',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './task-filters.component.html',
   styleUrl: './task-filters.component.scss',
 })
